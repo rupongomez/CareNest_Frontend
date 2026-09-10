c717653e4cb5d6aa3de00f676026e3171805f929
RuponGomeZ <anthonyrupon0@gmail.com>
2026-09-10 23:29:18 +0600

    first commit

---
+++ src/providers/index.tsx
@@ -0,0 +1,6 @@
+import { ReactNode } from "react";
+import QueryProvider from "./query.provider";
+
+export default function Providers({ children }: { children: ReactNode }) {
+  return <QueryProvider>{children}</QueryProvider>;
+}
