4c4efc2abca774058d57dcc99c4ddc7d64abb222
RuponGomeZ <anthonyrupon0@gmail.com>
2026-09-21 17:32:07 +0600

    dashboard side bar added

--- src/app/(dashboard)/admin/layout.tsx
+++ src/app/(dashboard)/admin/layout.tsx
@@ -1,6 +1,11 @@
 import RoleGuard from "@/components/auth/role-guard";
+import DashboardShell from "@/components/dashboard/dashboard-shell";
 import React, { ReactNode } from "react";
 
 export default function layout({ children }: { children: ReactNode }) {
-  return <RoleGuard roles={["ADMIN"]}>admin layout{children}</RoleGuard>;
+  return (
+    <RoleGuard roles={["ADMIN", "SUPER_ADMIN"]}>
+      <DashboardShell role="SUPER_ADMIN">{children}</DashboardShell>
+    </RoleGuard>
+  );
 }
